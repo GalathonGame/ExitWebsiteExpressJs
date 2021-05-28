0ceb8503e48e29daf33f69848122ecd809864cd9
Thanh Nhan Le <thanhnhanle1412@gmail.com>
2021-05-28 15:59:22 +0800

    Addding some dummy page for login

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,18 @@
 const functions = require("firebase-functions");
-
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 const port = 3000;
+const crsfMiddleware = csrf({ cookie: true });
+
 app.set("view engine", "html");
 app.engine("html", require("ejs").renderFile);
 app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(crsfMiddleware);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello Mah Friend");
@@ -13,10 +20,19 @@ app.use(express.static(__dirname + "/public"));
 app.get(["/", "/home", "/index"], (req, res) => {
   res.render("index.html");
 });
+
 app.get("/cart", (req, res) => {
   res.render("cart.html");
 });
-
+app.get("/profile", (req, res) => {
+  res.render("profile.html");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup.html");
+});
+app.get("/login", (req, res) => {
+  res.render("login.html");
+});
 app.listen(port, () => console.log("Listening on port " + port));
 
 exports.app = functions.https.onRequest(app);
